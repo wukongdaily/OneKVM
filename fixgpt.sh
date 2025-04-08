bf4f63fdaeebf3b8be01aadaf555d1d29273eb5e
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-04-08 17:26:22 +0800

    Create fixgpt.sh

---
+++ fixgpt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e  
+
+sudo apt update -y
+sudo apt install -y gdisk
+sudo apt install -y expect
+
+DISK="/dev/sda"
+
+expect <<EOF
+spawn sudo gdisk "$DISK"
+expect "Command (? for help):" { send "w\r" }
+expect "correct this problem? (Y/N):" { send "Y\r" }
+expect "Do you want to proceed? (Y/N):" { send "Y\r" }
+expect eof
+EOF
+
+echo "GPT分区表已成功写入 $DISK"
